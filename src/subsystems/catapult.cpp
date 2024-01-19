ba371f79581f558b9b7d83f96530d95341f15efc
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2024-01-19 14:44:22 -0600

    Implement state machine for auton

--- src/subsystems/catapult.cpp
+++ src/subsystems/catapult.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include "constants.h"
 #include "comets/math.h"
+#include "comets/logger.h"
 
 static inline constexpr double IDLE_VELOCITY_ERROR_RANGE = 10.0;
 static inline constexpr double IDLE_POSITION_ERROR_RANGE = 10.0;
@@ -12,7 +13,7 @@ namespace arm = constants::catapult;
 static double get_next_nearest_position(double curr, double target);
 static double remap_360_to_ps180(double curr);
 
-Catapult::Catapult() : m_motor(arm::PORT), targetPositionVelocity({0,0}), movingToPosition(false)
+Catapult::Catapult() : m_motor(arm::PORT), targetPositionVelocity({0, 0}), movingToPosition(false)
 {
     // m_motor.setPosPID(arm::POS_PIDF.F, arm::POS_PIDF.P, arm::POS_PIDF.I, arm::POS_PIDF.D);
     // m_motor.setVelPID(arm::VEL_PIDF.F, arm::VEL_PIDF.P, arm::VEL_PIDF.I, arm::VEL_PIDF.D);
@@ -34,16 +35,17 @@ double Catapult::get_position()
 
 void Catapult::wind_back()
 {
-    if (fireAndWind) return;
+    if (fireAndWind)
+        return;
     const auto curr_pos = get_position();
     if (comets::in_range((fmod(curr_pos, 360)), -arm::TOLERANCE, arm::TOLERANCE))
     {
-        std::printf("catapult at zero.\n");
+        COMET_LOG("catapult at zero");
         return;
     }
 
     double nearestPosition = get_next_nearest_position(curr_pos, 0);
-    std::printf("pos curr %f ; near %f\n", curr_pos, nearestPosition);
+    COMET_LOG("pos curr %f ; near %f", curr_pos, nearestPosition);
     targetPositionVelocity = {nearestPosition + 15, 50};
     movingToPosition = true;
     // std::printf("done winding.\n");
@@ -51,7 +53,8 @@ void Catapult::wind_back()
 
 void Catapult::fire()
 {
-    if (fireAndWind) return;
+    if (fireAndWind)
+        return;
     m_motor.moveVelocity(80);
     movingToPosition = true;
     targetPositionVelocity = {get_next_nearest_position(get_position(), 80), 80};
@@ -97,7 +100,7 @@ void Catapult::periodic()
         {
             m_motor.moveVelocity(0);
             movingToPosition = false;
-            std::printf("done moving to position %f.\n", targetPositionVelocity.first);
+            COMET_LOG("done moving to position %f", targetPositionVelocity.first);
 
             if (fireAndWind)
             {
@@ -113,7 +116,7 @@ static double get_next_nearest_position(double curr, double target)
     const double remainder = fmod((360.0 + target) - fmod(curr, 360.0), 360.0);
     const double new_target = curr + remainder;
     if (new_target < curr)
-        printf("new_target (%f) < curr (%f); assertion error:", new_target, curr);
+        COMET_LOG("new_target (%f) < curr (%f); assertion error:", new_target, curr);
     assert(new_target >= curr);
     return new_target;
 }
--- src/tasks/auton.cpp
+++ src/tasks/auton.cpp
@@ -2,7 +2,20 @@
 #include "comets/vendor.h"
 #include "tasks/auton.h"
 
+#include "comets/logger.h"
+
 using namespace okapi;
+
+static constexpr auto FEEDING_HOLD_DURATION = 300_ms;
+static constexpr auto FIRING_HOLD_DURATION = 300_ms;
+
+enum class AutonState
+{
+    GOTO_FEEDING = -1,
+    CURR_FEEDING = 0, // This should be the initial condition
+    GOTO_FIRING = 1,
+    CURR_FIRING = 2,
+};
 
 void autonomous_initialize()
 {
@@ -21,4 +34,53 @@ void autonomous_initialize()
  */
 void autonomous()
 {
+    auto state = AutonState::CURR_FEEDING;
+    bool firstChange = false;
+
+    Timer timer;
+    intake->forward();
+    timer.placeMark();
+
+    const auto changeStateAfter = [&](AutonState v, QTime duration)
+    {
+        if (timer.getDtFromMark() >= duration)
+        {
+            timer.placeMark();
+            state = v;
+            return true;
+        }
+        return false;
+    };
+
+    while (true)
+    {
+        catapult->periodic();
+        COMET_LOG("%d", int(state));
+        COMET_LOG("%0.2f ms since mark", timer.getDtFromMark().convert(okapi::millisecond));
+        switch (state)
+        {
+        case AutonState::CURR_FEEDING:
+        {
+            changeStateAfter(AutonState::GOTO_FIRING, FEEDING_HOLD_DURATION);
+            break;
+        }
+        case AutonState::CURR_FIRING:
+        {
+            catapult->fire_and_wind();
+            changeStateAfter(AutonState::GOTO_FEEDING, FIRING_HOLD_DURATION);
+            break;
+        }
+        case AutonState::GOTO_FEEDING:
+        {
+            catapult->wind_back();
+            changeStateAfter(AutonState::CURR_FEEDING, 500_ms);
+            break;
+        }
+        case AutonState::GOTO_FIRING:
+        {
+            changeStateAfter(AutonState::CURR_FIRING, 500_ms);
+            break;
+        }
+        }
+    }
 }
